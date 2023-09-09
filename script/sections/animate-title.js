3532a12d8e22a878a4b6d94ed81293d98d118bda
Yauheny Bychkou <375445433672@yandex.by>
2023-09-09 21:53:04 +0300

    fix: animate title

--- script/sections/animate-title.js
+++ script/sections/animate-title.js
@@ -1,18 +1,17 @@
 const initAnimateAfterLoadPage = () => {
   window.onload = () => {
     const title = document.querySelector(".title-animate--start");
-    animateTitle(title);
+    animateTitle(title, []);
   };
 };
 
 const initAnimateTitleBeforeScroll = () => {
-  let count = 0;  
+  const titleArray = [];
   const observer = new IntersectionObserver(
     (enties) => {
       enties.forEach((entry) => {
-        if (entry.isIntersecting && count === 0) {
-          animateTitle(entry.target);
-          count++;
+        if (entry.isIntersecting) {
+          animateTitle(entry.target, titleArray);
         }
       });
     },
@@ -27,19 +26,20 @@ const initAnimateTitleBeforeScroll = () => {
 };
 
 
-const animateTitle =(title)=>{
-  const textTitle = title.dataset.title.split("");
-    textTitle.forEach((letter, idx) => {
-      const newSpan = document.createElement("span");
-
-      newSpan.innerHTML = letter;
-      newSpan.classList.add("letter", "letter--hidden");
-      title.append(newSpan);
-        setTimeout(() => {
-          newSpan.classList.remove("letter--hidden");
-        },  15 * idx);
-      
-    });
+const animateTitle =(title, titleArray)=>{
+  if(!titleArray.includes(title)) {
+    titleArray.push(title)
+    const textTitle = title.dataset.title.split("");
+      textTitle.forEach((letter, idx) => {
+        const newSpan = document.createElement("span");
+        newSpan.innerHTML = letter;
+        newSpan.classList.add("letter", "letter--hidden");
+        title.append(newSpan);
+          setTimeout(() => {
+            newSpan.classList.remove("letter--hidden");
+          },  15 * idx);
+      });
+  } 
 }
 
 if (document.querySelector(".title")) {
